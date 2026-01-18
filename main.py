e53c71ca342d6fd562c4c05fd20b377fa3e88301
Salman Ahmad <salmanahmad111499@gmail.com>
2026-01-18 23:57:07 +0000

    cleaned main.py from rubbish code, and seperated different components, linked them using handlers, and managed main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+
+from PDF_Automation import GUI
+from PDF_Automation import PDFAutomation
+
+if __name__ == "__main__":
+    # PDF Automation object
+    pdf_automation = PDFAutomation()
+    pdf_automation.run()
